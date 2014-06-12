4dfe6171cf3bc855a98cd99c576d6c00198fc811
holygunner <djholygunner@gmail.com>
2014-06-13 00:23:56 +0300

    black&white; multithreads

--- ImageDecoder/src/com/image_convertor/AppThread.java
+++ ImageDecoder/src/com/image_convertor/AppThread.java
@@ -1,6 +1,9 @@
 package com.image_convertor;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -11,15 +14,13 @@ public class AppThread extends Thread {
     private static int threadsCount;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        String filePath = "U:/! JAVA vol.2/- задания и повтор теории (с 10-06-14)/1 Threads/Black&White Image Convertor by MultiThreads/image/Daft+Punk+sweet.jpg";
-        int threadsCount = 4;
+        String filePath = "U:/! JAVA vol.2/- задания и повтор теории (с 10-06-14)/1 Threads/Black&White Image Convertor by MultiThreads Fix/image/28CF6A39A11B-22.jpg";
+        int threadsCount = 10;
 
 
         img = ImageInput.getImage(filePath);
         AppThread.filePath = filePath;
         AppThread.threadsCount = threadsCount;
-
-//        ImageOutput.setImage(filePath);
 
         ArrayList<AppThread> appThreadsList = new ArrayList<AppThread>();
         for (int i=0; i<threadsCount; ++i) {
@@ -37,10 +38,34 @@ public class AppThread extends Thread {
     public void run() {
 
         int currentThreadNumber = Integer.parseInt(Thread.currentThread().getName());
-//        System.out.println(threadN);
 
         try {
-            ImageBlackAndWhiteConverter.convertColor(currentThreadNumber);
+
+            int red, green, blue;
+            int newColor;
+
+            int threadsCount = AppThread.getThreadsCount();
+            BufferedImage originalImage = AppThread.getImg();
+
+            int heightFirst = ((originalImage.getHeight())/threadsCount)*(currentThreadNumber-1);
+            int heightLast = ((originalImage.getHeight())/threadsCount)*currentThreadNumber;
+
+
+        System.out.println(heightFirst + "-" + heightLast + "-" + currentThreadNumber);
+        System.out.println("image width = " + originalImage.getHeight());
+
+            for (int h=heightFirst; h < heightLast; ++h) {
+                for (int w=0; w < originalImage.getWidth(); ++w) {
+                    red = new Color(originalImage.getRGB(w,h)).getRed();
+                    green = new Color(originalImage.getRGB(w,h)).getGreen();
+                    blue = new Color(originalImage.getRGB(w,h)).getBlue();
+                    newColor = ((red+green+blue)/3);
+                    Color color = new Color(newColor, newColor, newColor);
+                    originalImage.setRGB(w,h,color.getRGB());
+                }
+            }
+
+            ImageIO.write(originalImage, "PNG", new File(AppThread.getFilePath() + "Black&WhiteEdit" + ".png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
